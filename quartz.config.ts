18aa3a718edb855e80dc057298163e2adb1c955e
Phos Mankelow <joshua@mankelow.org>
2024-08-04 17:45:06 +0100

    Quartz sync: Aug 4, 2024, 5:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "phosphorescent.dev",
+    pageTitle: "Phos' Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
