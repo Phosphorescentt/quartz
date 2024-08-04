0a2907d70c243150fd0cc6f706463f79badcd70f
Phos Mankelow <joshua@mankelow.org>
2024-08-04 17:21:23 +0100

    Quartz sync: Aug 4, 2024, 5:21 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,22 +8,22 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "phosphorescent.dev",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: null,
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "phosphorescent.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Baskerville",
+        body: "Libre Baskerville",
         code: "IBM Plex Mono",
       },
       colors: {
